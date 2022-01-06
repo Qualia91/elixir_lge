29a8fddbce5adce7ccddf7e6a2e7fe60425c4de2
BOC Dev <53903568+Qualia91@users.noreply.github.com>
2022-01-06 15:01:53 +0000

    example of compiling and loading c code at run

--- lib/elixir_lge/application.ex
+++ lib/elixir_lge/application.ex
@@ -10,6 +10,18 @@ defmodule Test do
   end
 end
 
+defmodule VulkanRenderer do
+  @on_load :load_nifs
+
+  def load_nifs do
+    :erlang.load_nif('./dll/vulkan_renderer', 0)
+  end
+
+  def vulkan_window(width, height) do
+    raise "NIF vulkan_window/2 not implemented"
+  end
+end
+
 defmodule ElixirLGE.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
@@ -21,13 +33,14 @@ defmodule ElixirLGE.Application do
   def start(_type, _args) do
 
     IO.inspect Test.add(1,2), label: "This was calculated in the dll"
+    IO.inspect VulkanRenderer.vulkan_window(800, 600), label: "Return of renderer: "
 
-    {_, _, _, pid} = ElixirLGE.Window.start_link
-    ElixirLGE.Gui.start_link
+    # {_, _, _, pid} = ElixirLGE.Window.start_link
+    # ElixirLGE.Gui.start_link
 
     children = [
       # Starts a worker by calling: App.Worker.start_link(arg)
-      {ElixirLGE.Boids, pid}
+      # {ElixirLGE.Boids, pid}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
